1499290ea0f78c698186bb6adce3f8ba2709fbd8
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2025-05-08 21:36:44 +0200

    Update Pop3.xaml.cs

--- WpfApp1/Pop3.xaml.cs
+++ WpfApp1/Pop3.xaml.cs
@@ -1,38 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace WpfApp1
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Pop3.xaml
-    /// </summary>
     public partial class Pop3 : Window
     {
-        public Pop3()
+        private Window rodzic; 
+
+        public Pop3(Window parent)
         {
             InitializeComponent();
+            rodzic = parent;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Close();
-            //ZAMKNAC POP1
+            
+            rodzic.Close();
+            this.Close();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Close();
+            this.Close(); 
         }
     }
 }
